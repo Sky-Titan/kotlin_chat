c9afa5cf647eb4df4d7c23ccad3c36af91ff1d9b
Sky-Titan <er65119@naver.com>
2020-04-19 16:48:48 +0900

    nothing

--- client/app/src/main/java/org/techtown/kotlinchat/Activity/SignUpActivity.kt
+++ client/app/src/main/java/org/techtown/kotlinchat/Activity/SignUpActivity.kt
@@ -15,7 +15,7 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-
+        
         signup_btn_signup.setOnClickListener(View.OnClickListener {
                 
         })
